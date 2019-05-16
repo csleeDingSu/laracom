32eeaf4eec87caed49edec7a5b85d5e16eff910b
csleeDingsu <cslee.dingsu@gmail.com>
2019-05-16 15:58:00 +0800

    update init

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
 
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Cashier::useCurrency(config('cart.currency'), config('cart.currency_symbol'));
     }
 
